4c2db9e9a02a49b035784d3e07e1c91cda781758
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2024-05-22 16:26:24 +0200

    third commit

---
+++ produkt-catalog-service-starwars/src/test/java/ch/blsyea/produktcatalogservicestarwars/ProduktCatalogServiceStarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.blsyea.produktcatalogservicestarwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduktCatalogServiceStarwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
